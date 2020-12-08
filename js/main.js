132d03a87c1324a87cb4a79a92e100382d04207a
Jeongeeun <53941317+jeingwi19@users.noreply.github.com>
2020-12-08 19:48:09 +0900

    201208 업로드2

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+    //#cnt1 스와이퍼
     var swiperCnt1 = new Swiper('#cnt1 .swiper-container', {
         // Optional parameters
         direction: 'horizontal',
@@ -23,11 +24,21 @@ $(document).ready(function(){
           return false;
       });
 
-      //#cnt1 자동실행 ㅋ르릭
+      //#cnt1 자동실행 클릭
       $('#cnt1 .btn_controller .autoplay').on('click', function(){
           $(this).hide().siblings().show();
           swiperCnt1.autoplay.start();
           return false;
       });
+
+    //#cnt2 스와이퍼
+    var swiperCnt2 = new Swiper('#cnt2 .swiper-container', {
+        direction: 'horizontal',
+
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+          },
+    })
 
 });
