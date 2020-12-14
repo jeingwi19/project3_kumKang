3ca87cf08a19950550fc639852c001834e089b4c
Jeongeeun <53941317+jeingwi19@users.noreply.github.com>
2020-12-14 09:29:02 +0900

    201214 업로드

--- js/common2.js
+++ js/common2.js
@@ -3,17 +3,15 @@ $(document).ready(function(){
     $('#menuWrap').hide();
 
     //pc 전체메뉴 버튼 클릭하면 전체depth2 나타나기
-    $('.header .util .menu .btn_open').on('click', function(){
-        if(!$(this).hasClass('active')) {
-            $(this).addClass('active').parents('.header').addClass('on').find('.util').next().stop().slideDown('fast', function(){
-                $(this).prev().find('.scr_text').text('전체메뉴 닫기');
-            });
+    $('#pcHeader .util .menu .btn_open').on('click', function(){
+        //메뉴에서 포커스가 나가지 않도록 제어??..
+        var _first = $('#menuWrap').find('[data-link="first"]');
+        var _last = $('#menuWrap').find('[data-link="last"]');
 
-            //메뉴에서 포커스가 나가지 않도록 제어??..
-            var _first = $('#menuWrap').find('[data-link="first"]');
-            var _last = $('#menuWrap').find('[data-link="last"]');
-
-            $('#menuWrap').css({display: 'block', width: '100%', height: '680px'}).stop().animate({top: 100}, -300, function(){
+        if(!$(this).hasClass('active')) { //닫겨진 경우
+            //on과 active 동시에 추가
+            $(this).addClass('active').parents('#pcHeader').addClass('on').find('#menuWrap').stop().slideDown('fast', function(){
+                $(this).prev().prev().find('.scr_text').text('전체메뉴 닫기');
                 _first.focus();
             });
 
@@ -32,11 +30,11 @@ $(document).ready(function(){
                 }
             });
 
-        } else if ($(this).hasClass('active')) {
-            $(this).removeClass('active').parents('.util').next().stop().slideUp('fast', function(){
-                $(this).prev().find('.scr_text').text('전체메뉴 열기');
+        } else if ($(this).hasClass('active')) { //열려진 경우
+            $(this).removeClass('active').parents('.util').next().next().stop().slideUp('fast', function(){
+                $(this).prev().prev().find('.scr_text').text('전체메뉴 열기');
+                if (!$('#pcHeader').hasClass('active')) $('#pcHeader').removeClass('on');
             });
-
         }
         /* else if(!$('.header .util .menu .btn_open').hasClass('active')) {
             //풀페이지 첫번째 body태그 활성화 클래스명 .fp-viewing-0
@@ -44,16 +42,17 @@ $(document).ready(function(){
         } */
     });
 
-    //인디케이터 첫번째 클릭, .header에서 .on제거...??
-    $('#wrap + #fp-nav ul > li > a').on('click', function(){
-        $(this).parents('#fp-nav').prev().find('.header').removeClass('on');
-    });
+    //인디케이터 첫번째 클릭, .header에서 .on제거...?? => 이건 뭔가요?? 필요없는것 같은데...
+    // $('#wrap + #fp-nav ul > li > a').on('click', function(){
+    //     $(this).parents('#fp-nav').prev().find('.header').removeClass('on');
+    // });
 
     //pc상단 nav누르면 depth2메뉴 등장 & 스타일 변환..??
     $('#pcGnb ul li a').on('click', function(){
-        //alert();
-        
-        $('.btn_open').addClass('active').parents('.header').addClass('on').find('#menuWrap').stop().slideDown('fast');
+        $('#pcHeader .btn_open').addClass('active').parents('#pcHeader').addClass('on').find('#menuWrap').stop().slideDown('fast', function () {
+            $(this).prev().prev().find('.scr_text').text('전체메뉴 닫기');
+            $('#menuWrap [data-link="first"]').focus();
+        });
         return false;
     });
 });
